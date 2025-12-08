c255abf919c456cfabd3b938b6ca60e6a3d85e53
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2025-12-08 23:58:24 +0530

    Initial commit with day 1 solution

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod year2025;
+
+fn main() {
+    year2025::day1::part2();
+}
---
+++ src/year2025/day1.rs
@@ -0,0 +1,71 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+#[allow(dead_code)]
+pub fn part1() {
+    let file = File::open("input.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut lock = 50;
+    let mut ans = 0;
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let direction: u8 = line.as_bytes()[0];
+        let mut distance = line[1..].trim().parse::<i32>().unwrap();
+        if direction == b'L' {
+            distance = -distance;
+        }
+
+        lock = (lock + distance) % 100;
+        if lock < 0 {
+            lock += 100;
+        }
+
+        if lock == 0 {
+            ans += 1;
+        }
+    }
+
+    println!("{}", ans);
+}
+
+pub fn part2() {
+    let file = File::open("input.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut lock = 50;
+    let mut ans = 0;
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let direction: u8 = line.as_bytes()[0];
+        let mut distance = line[1..].trim().parse::<i32>().unwrap();
+        if direction == b'L' {
+            distance = -distance;
+        }
+
+        let old_lock = lock;
+        lock += distance;
+        if lock >= 100 {
+            ans += lock / 100;
+        } else if lock == 0 {
+            ans += 1;
+        } else if lock < 0 {
+            // Exclude the case when we were already on 0 before, cause then we didn't pass 0
+            if old_lock != 0 {
+                ans += 1;
+            }
+            ans += -lock / 100;
+        }
+
+        lock %= 100;
+        if lock < 0 {
+            lock += 100;
+        }
+    }
+
+    println!("{}", ans);
+}
